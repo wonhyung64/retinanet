e5ada032026144833f0be6c2f933987cf9d8b007
WonHyung Shin <wonhyung64@icloud.com>
2022-08-23 12:14:21 +0900

    feat - reconstruction

---
+++ run.py
@@ -0,0 +1,31 @@
+#%%
+import os
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+from tqdm import tqdm
+
+from module import (
+    initialize_process,
+    NEPTUNE_API_KEY,
+    NEPTUNE_PROJECT,
+    load_dataset,
+    build_dataset,
+)
+
+# %%
+if __name__ == "__main__":
+    args, run, weights_dir = initialize_process(
+        NEPTUNE_API_KEY, NEPTUNE_PROJECT
+    )
+
+    datasets, labels, train_num, valid_num, test_num = load_dataset(
+        name=args.name, data_dir=args.data_dir
+    )
+    train_set, valid_set, test_set = build_dataset(
+        datasets, args.batch_size, args.img_size
+    )
+# %%
