90c1bbbad9d826bfbe6df03233a5307e05ad7ef6
WonHyung Shin <wonhyung64@icloud.com>
2022-09-15 20:47:13 +0900

    chore - reconstruction

---
+++ module/model.py
@@ -0,0 +1,157 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import (
+    Layer,
+    Conv2D,
+    UpSampling2D,
+    ReLU,
+)
+
+
+def get_backbone():
+    backbone = tf.keras.applications.ResNet50(
+        include_top=False, input_shape=[None, None, 3]
+    )
+    c3_output, c4_output, c5_output = [
+        backbone.get_layer(layer_name).output
+        for layer_name in ["conv3_block4_out", "conv4_block6_out", "conv5_block3_out"]
+    ]
+
+    return tf.keras.Model(
+        inputs=[backbone.inputs], outputs=[c3_output, c4_output, c5_output]
+    )
+
+
+class FeaturePyramid(Layer):
+
+    def __init__(self, backbone=None, **kwargs):
+        super(FeaturePyramid, self).__init__(name="FeaturePyramid", **kwargs)
+        self.backbone = backbone if backbone else get_backbone()
+        self.conv_c3_1x1 = Conv2D(256, 1, 1, "same")
+        self.conv_c4_1x1 = Conv2D(256, 1, 1, "same")
+        self.conv_c5_1x1 = Conv2D(256, 1, 1, "same")
+        self.conv_c3_3x3 = Conv2D(256, 3, 1, "same")
+        self.conv_c4_3x3 = Conv2D(256, 3, 1, "same")
+        self.conv_c5_3x3 = Conv2D(256, 3, 1, "same")
+        self.conv_c6_3x3 = Conv2D(256, 3, 2, "same")
+        self.conv_c7_3x3 = Conv2D(256, 3, 2, "same")
+        self.upsample_2x = UpSampling2D(2)
+
+    def call(self, images, training=False):
+        c3_output, c4_output, c5_output = self.backbone(images, training=training)
+        p3_output = self.conv_c3_1x1(c3_output)
+        p4_output = self.conv_c4_1x1(c4_output)
+        p5_output = self.conv_c5_1x1(c5_output)
+        p4_output = p4_output + self.upsample_2x(p5_output)
+        p3_output = p3_output + self.upsample_2x(p4_output)
+        p3_output = self.conv_c3_3x3(p3_output)
+        p4_output = self.conv_c4_3x3(p4_output)
+        p5_output = self.conv_c5_3x3(p5_output)
+        p6_output = self.conv_c6_3x3(c5_output)
+        p7_output = self.conv_c7_3x3(tf.nn.relu(p6_output))
+
+        return p3_output, p4_output, p5_output, p6_output, p7_output
+
+
+def build_head(output_filters, bias_init):
+    head = tf.keras.Sequential([tf.keras.Input(shape=[None, None, 256])])
+    kernel_init = tf.initializers.RandomNormal(0.0, 0.01)
+    for _ in range(4):
+        head.add(
+            Conv2D(256, 3, padding="same", kernel_initializer=kernel_init)
+        )
+        head.add(ReLU())
+    head.add(
+        Conv2D(
+            output_filters,
+            3,
+            1,
+            padding="same",
+            kernel_initializer=kernel_init,
+            bias_initializer=bias_init,
+        )
+    )
+
+    return head
+
+
+class RetinaNet(Model):
+
+    def __init__(self, num_classes, backbone=None, **kwargs):
+        super(RetinaNet, self).__init__(name="RetinaNet", **kwargs)
+        self.fpn = FeaturePyramid(backbone)
+        self.num_classes = num_classes
+
+        prior_probability = tf.constant_initializer(-np.log((1 - 0.01) / 0.01))
+        self.cls_head = build_head(9 * num_classes, prior_probability)
+        self.box_head = build_head(9 * 4, "zeros")
+
+    def call(self, image, training=False):
+        features = self.fpn(image, training=training)
+        N = tf.shape(image)[0]
+        cls_outputs = []
+        box_outputs = []
+        for feature in features:
+            box_outputs.append(tf.reshape(self.box_head(feature), [N, -1, 4]))
+            cls_outputs.append(
+                tf.reshape(self.cls_head(feature), [N, -1, self.num_classes])
+            )
+        cls_outputs = tf.concat(cls_outputs, axis=1)
+        box_outputs = tf.concat(box_outputs, axis=1)
+        return tf.concat([box_outputs, cls_outputs], axis=-1)
+
+
+class DecodePredictions(tf.keras.layers.Layer):
+
+    def __init__(
+        self,
+        num_classes=20,
+        confidence_threshold=0.05,
+        nms_iou_threshold=0.5,
+        max_detections_per_class=100,
+        max_detections=100,
+        box_variance=[0.1, 0.1, 0.2, 0.2],
+        **kwargs
+    ):
+        super(DecodePredictions, self).__init__(**kwargs)
+        self.num_classes = num_classes
+        self.confidence_threshold = confidence_threshold
+        self.nms_iou_threshold = nms_iou_threshold
+        self.max_detections_per_class = max_detections_per_class
+        self.max_detections = max_detections
+
+        self._anchor_box = AnchorBox()
+        self._box_variance = tf.convert_to_tensor(
+            [0.1, 0.1, 0.2, 0.2], dtype=tf.float32
+        )
+
+    def _decode_box_predictions(self, anchor_boxes, box_predictions):
+        boxes = box_predictions * self._box_variance
+        boxes = tf.concat(
+            [
+                boxes[:, :, :2] * anchor_boxes[:, :, 2:] + anchor_boxes[:, :, :2],
+                tf.math.exp(boxes[:, :, 2:]) * anchor_boxes[:, :, 2:],
+            ],
+            axis=-1,
+        )
+        boxes_transformed = convert_to_corners(boxes)
+
+        return boxes_transformed
+
+    def call(self, images, predictions):
+        image_shape = tf.cast(tf.shape(images), dtype=tf.float32)
+        anchor_boxes = self._anchor_box.get_anchors(image_shape[1], image_shape[2])
+        box_predictions = predictions[:, :, :4]
+        cls_predictions = tf.nn.sigmoid(predictions[:, :, 4:])
+        boxes = self._decode_box_predictions(anchor_boxes[None, ...], box_predictions)
+
+        return tf.image.combined_non_max_suppression(
+            tf.expand_dims(boxes, axis=2),
+            cls_predictions,
+            self.max_detections_per_class,
+            self.max_detections,
+            self.nms_iou_threshold,
+            self.confidence_threshold,
+            clip_boxes=False,
+        )
