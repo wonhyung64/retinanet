8cd48bf9544484d67ec025b63905c689a53d5f89
WonHyung Shin <wonhyung64@icloud.com>
2022-09-22 20:40:30 +0900

    feat - tf.function experiment

--- attempt.py
+++ attempt.py
@@ -22,6 +22,7 @@ clf_loss_fn = RetinaNetClassificationLoss(args.alpha, args.gamma)
 optimizer = build_optimizer(args.batch_size, data_num=train_num)
 colors = tf.random.uniform((labels.num_classes, 4), maxval=256, dtype=tf.int32)
 
+#%%
 train_time = train(run, args.epochs, args.batch_size,
     train_num, valid_num, train_set, valid_set, labels,
     model, decoder, box_loss_fn, clf_loss_fn, optimizer, weights_dir)
@@ -29,3 +30,13 @@ train_time = train(run, args.epochs, args.batch_size,
 model.load_weights(f"{weights_dir}.h5")
 mean_ap, mean_evaltime = evaluate(run, test_set, test_num, model, decoder, labels, "test")
 record_result(run, weights_dir, train_time, mean_ap, mean_evaltime)
+
+# %%
+'''
+from module.optimize import forward_backward
+from module.loss import compute_loss
+input_img, true = next(train_set)
+pred = model(input_img)
+forward_backward(input_img, true, model, box_loss_fn, clf_loss_fn, optimizer, labels.num_classes)
+compute_loss(true, pred, box_loss_fn, clf_loss_fn)
+'''
